f61650900796c9acb41d397f57bc8a792e35a45e
KimKihyeon <kidone0033@gmail.com>
2023-06-20 14:32:33 +0900

    수정, 하나 조회 기능 로직 구현

---
+++ src/main/java/com/example/demo/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.web.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
